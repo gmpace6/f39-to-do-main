5a929d87bb053187c86b70e52d824fba07ab7d72
Garrett Pace <elderpace6@gmail.com>
2023-05-02 17:08:24 -0600

    update1

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 
-const cors = require('cors');
+const app = express()
+
+app.use(express.json)
+app.use(cors())
+
+app.listen(5478, () => console.log("Docked at port 5478"))
